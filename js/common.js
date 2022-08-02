ab8689c1b14d5596cd69bcc8ce47e1357b86414f
Arijit Bhakta <arijitbhakta611@gmail.com>
2022-08-02 12:44:42 +0530

    modified common.js

--- js/common.js
+++ js/common.js
@@ -5,12 +5,12 @@ window.addEventListener("load", function(){
         let signup_form_data=new FormData(signup_form);
         httpRequest.addEventListener("load", signup_success);
         httpRequest.addEventListener("error", on_error);
-        httpRequest.open('POST', 'user_authentication/signup_submit.php');
-        httpRequest.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+        httpRequest.open("POST", "user_authentication/signup_submit.php");
+        //httpRequest.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
         httpRequest.send(signup_form_data);
         document.getElementById("loading").style.display="block";
         event.preventDefault();
-    })
+    });
 
     let login_form=document.getElementById("login-form");
     login_form.addEventListener("submit", function(event){
@@ -19,28 +19,40 @@ window.addEventListener("load", function(){
         httpRequest.addEventListener("load", login_success);
         httpRequest.addEventListener("error", on_error);
         httpRequest.open('POST', 'user_authentication/login_submit.php');
-        httpRequest.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+        //httpRequest.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
         httpRequest.send(login_form_data);
         document.getElementById("loading").style.display="block";
         event.preventDefault();
-    })
-})
+    });
+});
 
-let signup_success=(event)=>{
+// let signup_success=function(event){
+//     document.getElementById("loading").style.display="none";
+//     let response=JSON.parse(event.target.responseText);
+//     if(response.success)
+//     {
+//         alert(response.message);
+//         window.location.href="index.php";
+//     }
+//     else
+//     {
+//         alert(response.message);
+//     }
+// }
+
+var signup_success=function(event){
     document.getElementById("loading").style.display="none";
-    let response=JSON.parse(event.target.responseText);
-    if(response.success)
-    {
+    var response=JSON.parse(event.target.responseText);
+    if(response.success){
         alert(response.message);
         window.location.href="index.php";
     }
-    else
-    {
-        alert("Something went wrong!");
+    else{
+        alert(response.message);
     }
-}
+};
 
-let login_success=(event)=>{
+let login_success=function(event){
     document.getElementById("loading").style.display="none";
     let response=JSON.parse(event.target.responseText);
     if(response.success)
@@ -50,11 +62,11 @@ let login_success=(event)=>{
     }
     else
     {
-        alert("Something went wrong");
+        alert(response.message);
     }
 };
 
-let on_error=()=>{
+let on_error=function(){
     document.getElementById("loading").style.display="none";
-    alert("Something wrent wrong!");
+    alert(response.message);
 };
